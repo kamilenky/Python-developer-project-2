df410a0ad50f8211f3f57df483a6dd0c30f29313
Kamila <kamilenkykamilenky@gmail.com>
2025-05-12 21:03:21 +0200

    Python-developer-project-2: deleted JonTenureShortened.py

--- JobTenureShortened.py
+++
@@ -1,6 +0,0 @@
-class JobTenureShortened:
-    def __init__(self, shortened_tenure:str, days: int) -> None:
-        self.shortened_tenure = shortened_tenure
-        self.days = days
-
-        # zvaliduje podmienku pre predlzenie zmluvy
