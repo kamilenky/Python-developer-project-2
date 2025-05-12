03a3147337fb3c640d00daf374cd682ef08489c0
Kamila <kamilenkykamilenky@gmail.com>
2025-05-12 20:44:15 +0200

    Python-developer-project-2: new module Requirements

---
+++ Requirements.py
@@ -0,0 +1,8 @@
+class Requirements:
+    def __init__(self, reasons: list) -> None:
+        self.reasons = reasons
+        #zostavi list podmienok pre pre predlzenie zmluvy/skratenie zmluvy
+
+    def list_requirements(self):
+        for reason in self.reasons:
+            reason.set_reason()
