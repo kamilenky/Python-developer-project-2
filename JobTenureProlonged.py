d66183b4b30d0cf1efbd7503eb3d8e21482e7a79
Kamila <kamilenkykamilenky@gmail.com>
2025-05-12 21:01:40 +0200

    Python-developer-project-2: renamed to job_tenure.py

--- JobTenureProlonged.py
+++
@@ -1,8 +0,0 @@
-class JobTenureProlonged:
-    def __init__(self, prolonged_tenure: str, days: int) -> None:
-        self.prolonged_tenure = prolonged_tenure
-        self.days = days
-        # zvaliduje podmienku pre predlzenie zmluvy
-
-    def set_reason(self) -> None:
-        print(f"Dovod nepritomnosti je {self.prolonged_tenure}")
