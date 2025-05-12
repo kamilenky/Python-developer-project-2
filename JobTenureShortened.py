fcb61b0bef92ab6494beef92b3c9813c5e2abd12
Kamila <kamilenkykamilenky@gmail.com>
2025-05-12 20:47:52 +0200

    Python-developer-project-2: new module JobTenureShortened

---
+++ JobTenureShortened.py
@@ -0,0 +1,6 @@
+class JobTenureShortened:
+    def __init__(self, shortened_tenure:str, days: int) -> None:
+        self.shortened_tenure = shortened_tenure
+        self.days = days
+
+        # zvaliduje podmienku pre predlzenie zmluvy
