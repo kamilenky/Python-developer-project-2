a4163c2f731adb02c6b4a12580e4fdffb3d029a3
Kamila <kamilenkykamilenky@gmail.com>
2025-05-17 21:05:24 +0200

    Python-developer-project-2: file in draft

--- requirements.py
+++ requirements.py
@@ -1,11 +1,12 @@
 class Requirements:
-    # premenna = x
     def __init__(self, reasons: list) -> None:
         self.reasons = reasons
+        pass
         #zostavi list podmienok pre pre predlzenie zmluvy/skratenie zmluvy
 
     def list_requirements(self):
         for reason in self.reasons:
             reason.set_reason()
+        pass
 
 
