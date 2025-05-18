586867881a5c8157e1b9c9643475abb482510d7b
Kamila <kamilenkykamilenky@gmail.com>
2025-05-18 21:22:59 +0200

    Python-developer-project-2: added part: # 1. the program asks for employee data input, displays employee data list

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-# 1. the program asks for employee data input, displays employee data list or raises the exceptions
+# 1. the program asks for employee data input, displays employee data list, raises the exceptions
+from employee_data import EmployeeData
+from employee_data import EmployeeList
 
 
+employee_data = [
+    EmployeeData(id=1, name="Asterix", surname= "Galois", job_classification="Opatrovateľ", contribution_amount_gross= 2000,
+     contribution_amount_net= 1620),
+    EmployeeData(id=2, name="Obelix", surname= "Gal", job_classification="Sociálny pracovník", contribution_amount_gross= 1500,
+     contribution_amount_net= 1215),
+    EmployeeData(id=3, name="Scoobee", surname= "Doo", job_classification="Inštruktor sociálnej rehabilitácie", contribution_amount_gross= 2000,
+     contribution_amount_net= 1620)
+]
+
+e = EmployeeList(employee_data)
+e.show_data()
