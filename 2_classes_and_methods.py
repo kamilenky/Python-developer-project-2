5b67b97d1a664fe9ed9e67330ecf8f8c0d194afd
Kamila <kamilenkykamilenky@gmail.com>
2025-05-18 21:14:38 +0200

    Python-developer-project-2: class EmployeeData removed to employee_data.py

--- 2_classes_and_methods.py
+++ 2_classes_and_methods.py
@@ -1,59 +1,65 @@
-# the program asks for employee data input, displays employee data list or raises the exceptions
-# input: y/n answer for displaying the data list
-# output: displays employee data list
+# # the program asks for employee data input, displays employee data list or raises the exceptions
+# # input: y/n answer for displaying the data list
+# # output: displays employee data list
+#
+# class EmployeeData:
+#     def __init__(self, data):
+#         self.data = data
+#
+#     def return_employee_data(self):
+#         for employee in employee_data:
+#             print(
+#                 f'{employee["name"]}, {employee["surname"]}, Pracovné zaradenie: {employee["job_classification"]}, '
+#                 f'Príspevok: {employee["contribution_amount_gross"]}, Príspevok po zdanení: {employee["contribution_amount_net"]}'
+#             )
+#
+#     def show_data(self):
+#         try:
+#             show_employees = input("Zobraziť zoznam zamestnancov (a/n): ").strip().lower()
+#
+#             if show_employees not in ["a","n"]:
+#                 raise ValueError ("Nespravny údaj. Zadajte iba 'a' alebo 'n'")
+#
+#             if show_employees == "a":
+#                 self.return_employee_data()
+#                 return show_employees
+#
+#             else:
+#                 print("Čauko, koniec programu.")
+#
+#         except ValueError as error:
+#             print("Nespravny údaj.", error)
+#
+#
+#     # output: zobrazi  tabulku  zamestnancov
+#
+#
+# employee_data = [
+#     {"name": "Asterix", "surname": "Galois", "job_classification": "Opatrovateľ", "contribution_amount_gross": 2000,
+#      "contribution_amount_net": 1620},
+#     {"name": "Obelix", "surname": "Gal", "job_classification": "Sociálny pracovník", "contribution_amount_gross": 1500,
+#      "contribution_amount_net": 1620},
+#     {"name": "Scoobee", "surname": "Doo", "job_classification": "Inštruktor sociálnej rehabilitácie",
+#      "contribution_amount_gross": 2000, "contribution_amount_net": 1215}
+# ]
+#
+# e = EmployeeData(employee_data)
+# e.show_data()
 
-class EmployeeData:
-    def __init__(self, data):
-        self.data = data
+# program zobrai zamestnanca a riradene data: meno, priezvisko, pracovne zaradenie, pracovny uvazok, vyska prispevku v hrubom,
+# vyska prispevku v cistom, zaciatok zmluvy, predpokladany koniec zmluvy
 
-    def return_employee_data(self):
-        for employee in employee_data:
-            print(
-                f'{employee["name"]}, {employee["surname"]}, Pracovné zaradenie: {employee["job_classification"]}, '
-                f'Príspevok: {employee["contribution_amount_gross"]}, Príspevok po zdanení: {employee["contribution_amount_net"]}'
-            )
-
-    def show_data(self):
-        try:
-            show_employees = input("Zobraziť zoznam zamestnancov (a/n): ").strip().lower()
-
-            if show_employees not in ["a","n"]:
-                raise ValueError ("Nespravny údaj. Zadajte iba 'a' alebo 'n'")
-
-            if show_employees == "a":
-                self.return_employee_data()
-                return show_employees
-
-            else:
-                print("Čauko, koniec programu.")
-
-        except ValueError as error:
-            print("Nespravny údaj.", error)
-
-
-    # output: zobrazi  tabulku  zamestnancov
-
-
-employee_data = [
-    {"name": "Asterix", "surname": "Galois", "job_classification": "Opatrovateľ", "contribution_amount_gross": 2000,
-     "contribution_amount_net": 1620},
-    {"name": "Obelix", "surname": "Gal", "job_classification": "Sociálny pracovník", "contribution_amount_gross": 1500,
-     "contribution_amount_net": 1620},
-    {"name": "Scoobee", "surname": "Doo", "job_classification": "Inštruktor sociálnej rehabilitácie",
-     "contribution_amount_gross": 2000, "contribution_amount_net": 1215}
-]
-
-e = EmployeeData(employee_data)
-e.show_data()
-
-
-def ReturnEmployeeName(self):
+class Employee
     pass
-    # get_id = input --> id / name
-    # print()
 
+    def choose_employee(self):
+        pass
 
-# zobrai zamestnanca a riradene data: meno, priezvisko, pracovne zaradenie, pracovny uvazok, vyska prispevku v hrubom, vyska prispevku v cistom, zaciatok zmluvy
+    def ReturnEmployeeName(self):
+        pass
+        # get_id = input --> id / name
+        # print()
+
 
 
 # amendement requirements
