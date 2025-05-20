58a624a62382bbc7df8e349df65565c3dd22ef2b
Kamila <kamilenkykamilenky@gmail.com>
2025-05-20 14:24:43 +0200

    Python-developer-project-2: added - except AppExceptions.InvalidInputError as error: (draft)

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from exceptions import AppExceptions
 
 
 employee_data = [
-    EmployeeData(id=1, name="Asterix", surname= "Galois", job_classification="Opatrovateľ", contribution_amount_gross= 2000,
+    EmployeeData(id=1, name="Astérix", surname= "Galois", job_classification="Opatrovateľ", contribution_amount_gross= 2000,
      contribution_amount_net= 1620),
-    EmployeeData(id=2, name="Obelix", surname= "Gal", job_classification="Sociálny pracovník", contribution_amount_gross= 1500,
+    EmployeeData(id=2, name="Obélix", surname= "Gal", job_classification="Sociálny pracovník", contribution_amount_gross= 1500,
      contribution_amount_net= 1215),
     EmployeeData(id=3, name="Scoobee", surname= "Doo", job_classification="Inštruktor sociálnej rehabilitácie", contribution_amount_gross= 2000,
      contribution_amount_net= 1620)
@@ -19,4 +19,4 @@ try:
     e.show_data()
 
 except AppExceptions.InvalidInputError as error:
-    print("Nespravny údaj.", error)
+    print("Nesprávny údaj.", error)
