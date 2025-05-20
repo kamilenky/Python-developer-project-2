4a7596b03238e48a9833650b24453f24e3578ead
Kamila <kamilenkykamilenky@gmail.com>
2025-05-20 14:18:37 +0200

    Python-developer-project-2: added - from exceptions import AppExceptions (draft)

--- employee_data.py
+++ employee_data.py
@@ -2,7 +2,7 @@
 # input: y/n answer for displaying the data list
 # output: displays employee data list
 
-from exceptions import InvalidInputError
+from exceptions import AppExceptions
 
 
 class EmployeeData:
@@ -31,7 +31,7 @@ class EmployeeList:
             show_employees = input("Zobraziť zoznam zamestnancov (a/n): ").strip().lower()
 
             if show_employees not in ["a","n"]:
-                raise InvalidInputError("Zadajte iba 'a' alebo 'n'")
+                raise AppExceptions.InvalidInputError("Zadajte iba 'a' alebo 'n'")
 
             if show_employees == "a":
                 self.return_employee_data()
@@ -40,21 +40,5 @@ class EmployeeList:
             else:
                 print("Čauko, koniec programu.")
 
-        # except ValueError as error:
-        #     print("Nespravny údaj.", error)
-
-
-
-# employee_data = [
-#     EmployeeData(id=1, name="Asterix", surname= "Galois", job_classification="Opatrovateľ", contribution_amount_gross= 2000,
-#      contribution_amount_net= 1620),
-#     EmployeeData(id=2, name="Obelix", surname= "Gal", job_classification="Sociálny pracovník", contribution_amount_gross= 1500,
-#      contribution_amount_net= 1215),
-#     EmployeeData(id=3, name="Scoobee", surname= "Doo", job_classification="Inštruktor sociálnej rehabilitácie", contribution_amount_gross= 2000,
-#      contribution_amount_net= 1620)
-# ]
-#
-# e = EmployeeList(employee_data)
-# e.show_data()
 
 
