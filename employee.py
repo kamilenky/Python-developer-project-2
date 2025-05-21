6f5b0ca7e5dc768d27c1405e98458b63b14e998e
Kamila <kamilenkykamilenky@gmail.com>
2025-05-21 15:31:49 +0200

    Python-developer-project-2: added - first draft (draft)

--- employee.py
+++ employee.py
@@ -5,17 +5,36 @@
 # output: displays employee data list
 
 from employee_data import EmployeeData
+from exceptions import AppExceptions
 
 class Employee:
     def __init__(self, employee):
         self.employee = employee
 
     def get_employee_by_id(self, employee_id):
+        try:
+            show_emp_id = inout("Zadajte id zamestnanca: ").strip().lower())
+
+
         for emp in self.employee:
             if id.employee_id == employee_id:
-                return emp
+                return
+
+    def show_emp_id(self):
+        show_emp_id = inout("Zadajte id zamestnanca: ").strip().lower())
+
+        if show_emp_id not in return_employee_data():
+            raise AppExceptions.ValueError("Neplatné id")
+
+        if show_emp_id == ...
+            self.get_employee_by_id
+            return show_emp_id
+
+
 
     def ReturnEmployeeName(self, employee_id):
         pass
         # get_id = input --> id / name
         # print()
+
+    def
