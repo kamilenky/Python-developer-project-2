f431bd768ad147a0a922784d8df6672dcb5b7cba
Kamila <kamilenkykamilenky@gmail.com>
2025-05-21 22:40:29 +0200

    Python-developer-project-2: update - def get_employee_by_id (draft)

--- employee.py
+++ employee.py
@@ -4,48 +4,50 @@
 # input: y/n answer for displaying the data list
 # output: displays employee data list
 
-from emplmployee_data import EmployeeData
+from employee_data import EmployeeData
 from employee_data import EmployeeList
 from exceptions import AppExceptions
+from main import List
 
 
 class Employee:
     def __init__(self, employee):
         self.employee = employee
 
-    def get_employee_by_id(self, employee_id):
+    def get_employee_by_id(self):
         try:
             emp_id = int(input("Zadajte ID zamestnanca: ").strip().lower())
-            selected_emp = next(e for e in employee_data if  e.id = emp_id, None)
+            selected_emp = next((e for e in EmployeeData if  e.id == emp_id), None)
 
             if selected_emp:
-                EmployeeList.return_employee_data()
+                return EmployeeList.return_employee_data()
 
             else: print('Neplatné ID.')
-        raise AppExceptions.ValueError("Zadajte ID zo zoznamu.!)
+        except AppExceptions.ValueError:
+            print("Zadajte ID zo zoznamu.")
 
 
 
-
-        for emp in self.employee:
-            if id.employee_id == employee_id:
-                return
-
-    def show_emp_id(self):
-        show_emp_id = int(input("Zadajte ID zamestnanca: ").strip().lower())
-
-        if show_emp_id not in return_employee_data():
-            raise AppExceptions.ValueError("Neplatné id")
-
-        if show_emp_id == ...
-            self.get_employee_by_id
-            return show_emp_id
-
-
-
-    def ReturnEmployeeName(self, employee_id):
-        pass
-        # get_id = input --> id / name
-        # print()
-
-    def
+        #
+        # for emp in self.employee:
+        #     if id.employee_id == employee_id:
+        #         return
+    #
+    # def show_emp_id(self):
+    #     show_emp_id = int(input("Zadajte ID zamestnanca: ").strip().lower())
+    #
+    #     if show_emp_id not in return_employee_data():
+    #         raise AppExceptions.ValueError("Neplatné id")
+    #
+    #     if show_emp_id == ...
+    #         self.get_employee_by_id
+    #         return show_emp_id
+    #
+    #
+    #
+    # def ReturnEmployeeName(self, employee_id):
+    #     pass
+    #     # get_id = input --> id / name
+    #     # print()
+    #
+    # def
