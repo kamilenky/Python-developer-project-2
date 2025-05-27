05bbc68dfbed81472941d4cb61c14bf3a6b6a2c0
Kamila <kamilenkykamilenky@gmail.com>
2025-05-27 18:26:32 +0200

    Python-developer-project-2: new file requirements.py added- class Requirements: class RequirementsProlongedTenure: class RequirementsShortenedTenure:
    (draft)

--- requirements.py
+++ requirements.py
@@ -1,7 +1,26 @@
 class Requirements:
-    def __init__(self, reasons: list) -> None:
-        self.reasons = reasons
-        pass
+    class RequirementsProlongedTenure:
+        def __init__(self, id, prolonged_tenure: list) -> None:
+            self.id = id
+            self.prolonged_tenure = prolonged_tenure
+
+        def __str__(self):
+           return (f'n\"Vyberte dôvod: "
+                    f"{self.id}, {self.prolonged_tenure}")
+
+    class RequirementsShortenedTenure:
+        def __init__(self, id, shortened_tenure: list ):
+            self.id = id
+            self.shortened_tenure = shortened_tenure
+
+
+
+        def __str__(self):
+            return (f"n\"Vyberte dôvod: "
+                    f"{self.id},{self.shortened_tenure}")
+            pass
+
+
         #zostavi list podmienok pre pre predlzenie zmluvy/skratenie zmluvy
 
     def list_requirements(self):
