afb8747e2e10861d40b5f27065eaa58dce0242f8
Kamila <kamilenkykamilenky@gmail.com>
2025-05-27 22:18:16 +0200

    Python-developer-project-2: added- list reasons_1, reasons_2 def show_requirements class RequirementsById:
    dry run
    
    (draft)

--- requirements.py
+++ requirements.py
@@ -2,34 +2,52 @@
 from exceptions import AppExceptions
 
 
+
+
 class RequirementsProlongedTenure:
-    def __init__(self, id, prolonged_tenure: list) -> None:
+    def __init__(self, id, prolonged_tenure) -> None:
         self.id = id
         self.prolonged_tenure = prolonged_tenure
 
     def __str__(self):
-        return f"\nVyberte dôvod: \n{self.id}, {self.prolonged_tenure}"
+        return f"\nVyberte dôvod - bla bla: \n{self.id}, {self.prolonged_tenure}"
 
 
 class RequirementsShortenedTenure:
-    def __init__(self, id, shortened_tenure: list):
+    def __init__(self, id, shortened_tenure) -> None:
         self.id = id
         self.shortened_tenure = shortened_tenure
 
     def __str__(self):
-        return f"\n Vyberte dôvod: \n {self.id}, {self.shortened_tenure}"
+        return f"\n Vyberte dôvod - ble ble: \n {self.id}, {self.shortened_tenure}"
 
 
 class RequirementsList:
-    def __init__(self):
+    def __init__(self, requirements_list):
         self.requirements_list = requirements_list
+
 
     def return_requirements_list(self):
         for rl in self.requirements_list:
             print(rl)
 
+    def show_requirements(self):
 
-class Requirements:
+        show_requirements = input("Zobraziť dôvody ukončenia zmluvy? (a/n): ").strip().lower()
+
+        if show_requirements not in ["a", "n"]:
+                raise AppExceptions.InvalidInputError("Zadajte iba 'a' alebo 'n'")
+
+        if show_requirements == "a":
+                self.return_requirements_list()
+                return show_requirements
+        else:
+            print("ešte neviem, kam to presmerujem")
+            exit()
+
+
+
+class RequirementsById:
     def __init__(self):
         self.requirements = requirements
 
@@ -46,3 +64,20 @@ class Requirements:
 
         except AppExceptions.ValueError:
             print("Zadajte číslo zo zoznamu.")
+
+reasons_1 = [
+    RequirementsProlongedTenure(id=1, prolonged_tenure="ospravedlnená neprítomnosť zamestnanca"),
+    RequirementsProlongedTenure(id=2, prolonged_tenure="výkon mimoriadnej služby")
+]
+
+
+reasons_2 = [
+    RequirementsShortenedTenure(id=1, shortened_tenure="zamestnanec je preradený na inú pracovnú pozíciu"),
+    RequirementsShortenedTenure(id=2, shortened_tenure="okamžité skončenie pracovného pomeru zamestnávateľom")
+]
+
+# requirements.py section
+req_1 = RequirementsList(reasons_1)
+req_2 = RequirementsList(reasons_2)
+req_1.show_requirements()
+req_2.show_requirements()
