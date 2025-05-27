2fc7fb02d6f6f6a2e5a0d824af0fbb58377846f0
Kamila <kamilenkykamilenky@gmail.com>
2025-05-27 22:20:12 +0200

    Python-developer-project-2: cleaning
    (draft)

--- employee.py
+++ employee.py
@@ -4,22 +4,22 @@
 # input: y/n answer for displaying the data list
 # output: displays employee data list
 
-from employee_data import EmployeeData
-from employee_data import EmployeeList
+from employee_data import EmployeeData, EmployeeList
 from exceptions import AppExceptions
 
 
 
 
 class Employee:
-    def __init__(self):
-        self.employee_data = employee_data
+    def __init__(self, employee):
+        self.employee = employee
+
 
 
     def get_employee_by_id(self):
         try:
             emp_id = int(input("Zadajte ID zamestnanca: ").strip().lower())
-            selected_emp = next((e for e in employee_data if  e.id == emp_id), None)
+            selected_emp = next((e for e in self.employee_data if  e.id == emp_id), None)
 
             if selected_emp:
                 return EmployeeList.return_employee_data()
