72b036df9a01097813ac80bf097c6c6d861c7797
Kamila <kamilenkykamilenky@gmail.com>
2025-05-29 11:53:44 +0200

    Python-developer-project-2: update - start_date in class EmployeeData
    (draft)

--- employee_data.py
+++ employee_data.py
@@ -7,17 +7,20 @@ from exceptions import AppExceptions
 
 
 class EmployeeData:
-    def __init__(self, id, name, surname, job_classification, contribution_amount_gross, contribution_amount_net):
+    def __init__(self, id, name, surname, job_classification, contribution_amount_gross, contribution_amount_net,
+                 start_date):
         self.id = id
         self.name = name
         self.surname = surname
         self.job_classification = job_classification
         self.contribution_amount_gross = contribution_amount_gross
         self.contribution_amount_net = contribution_amount_net
+        self.start_date = start_date
 
     def __str__(self):
-       return  (f'Zamestnanec: {self.id},{self.name} {self.surname}, Pracovné zaradenie: {self.job_classification}, '
-                f'Príspevok: {self.contribution_amount_gross}, Príspevok po zdanení: {self.contribution_amount_net}')
+        date_str = self.start_date.strftime("%d-%m-%y") if self.start_date else "%Y-%m-%d"
+        return (f'Zamestnanec: {self.id},{self.name} {self.surname}, Pracovné zaradenie: {self.job_classification},'
+            f'Príspevok: {self.contribution_amount_gross}, Príspevok po zdanení: {self.contribution_amount_net}, Začiatok zmluvy: {self.start_date}')
 
 
 class EmployeeList:
@@ -26,24 +29,18 @@ class EmployeeList:
 
     def return_employee_data(self):
         for employee in self.employee_list:
-           print(employee)
+            print(employee)
 
     def show_data(self):
 
-            show_employees = input("Zobraziť zoznam zamestnancov (a/n): ").strip().lower()
+        show_employees = input("Zobraziť zoznam zamestnancov (a/n): ").strip().lower()
 
-            if show_employees not in ["a","n"]:
-                raise AppExceptions.InvalidInputError("Zadajte iba 'a' alebo 'n'")
+        if show_employees not in ["a", "n"]:
+            raise AppExceptions.InvalidInputError("Zadajte iba 'a' alebo 'n'")
 
-            if show_employees == "a":
-                self.return_employee_data()
-                return show_employees
+        if show_employees == "a":
+            self.return_employee_data()
+            return show_employees
 
-            else:
-                print("...")
-
-
-
-
-
-
+        else:
+            print("...")
