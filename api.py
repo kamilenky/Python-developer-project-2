e3dfdc80f9bccc121d9e5e12d29b7c6da6631042
Kamila <kamilenkykamilenky@gmail.com>
2025-05-30 14:14:49 +0200

    Python-developer-project-2: added description

--- api.py
+++ api.py
@@ -1,12 +1,17 @@
+# tato cast prekonvertuje sumu na vratenie na inu menu pomocou API
+# nie je dokoncena
+# chcem prepojit vystup {refund_amount:.2f} z calculations.py a amount_in_currency
+
 import requests
 
-def get_exchange_rate(base="EUR", target="USD"):
+
+def get_exchange_rate(base="EUR", target="CZK"):
     url = f"https://api.exchangerate-api.com/v4/latest/{base}"
     response = requests.get(url)
     data = response.json()
     return data["rates"].get(target)
 
 # Usage
-rate = get_exchange_rate("EUR", "USD")
-amount_in_usd = 1000 * rate
-print(f"Refund in USD: {amount_in_usd:.2f}")
+rate = get_exchange_rate("EUR", "CZK")
+amount_in_currency =  1 * rate
+print(f"Refund in CZK: {amount_in_currency:.2f}")
