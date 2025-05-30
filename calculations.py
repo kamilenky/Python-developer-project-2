abced3d66f92a04224b7bca007dc8a2fb6b9ddb7
Kamila <kamilenkykamilenky@gmail.com>
2025-05-30 12:44:46 +0200

    Python-developer-project-2: update - class CalculateRefund:

--- calculations.py
+++ calculations.py
@@ -1,8 +1,5 @@
 from datetime import datetime, timedelta
 
-
-
-
 base = 1096
 contract_duration = 3*365
 remaining_part = base - contract_duration
@@ -15,5 +12,17 @@ refund_amount = remaining_part*amount_day_net
 
 class CalculateRefund:
     def __init__(self):
-        pass
+        self.start_date = datetime(2025, 1, 1)
+        self.end_date = self.start_date + timedelta(days=self.contract_duration)
+        self.contract_duration = 3*365
+        self.refund_amount = refund_amount
+
+    def calculate_refund(self):
+        try:
+            end_date_input = input("Zadajte dátum ukončenia zmluvy: ").strip()
+            end_date = datime.strptime(end_date_input, "%d-%m-%y")
+
+
+
+
 
