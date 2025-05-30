00ba2e0a429d1313c4c64a150bb46c799de05b41
Kamila <kamilenkykamilenky@gmail.com>
2025-05-30 14:21:28 +0200

    Python-developer-project-2: added description

--- calculations.py
+++ calculations.py
@@ -1,26 +1,68 @@
+# tato cast vypocita sumu na vratenie zamestnavatelovi
+# nie je dokoncena
+# chcem pridat dalsie polozky do def print_summary
+# chcem prepojit vstupy z employee_data, requirements
+
 from datetime import datetime, timedelta
-
-base = 1096
-contract_duration = 3*365
-remaining_part = base - contract_duration
-
-
-contribution_amount_net = 1215
-amount_day_net = contribution_amount_net/contract_duration
-refund_amount = remaining_part*amount_day_net
 
 
 class CalculateRefund:
-    def __init__(self):
-        self.start_date = datetime(2025, 1, 1)
-        self.end_date = self.start_date + timedelta(days=self.contract_duration)
+    def __init__(self, start_date: datetime, total_paid: float):
+        self.start_date = start_date
         self.contract_duration = 3*365
         self.refund_amount = refund_amount
+        self.total_paid = total_paid
+        self.end_date = self.start_date + timedelta(days=self.contract_duration)
 
     def calculate_refund(self):
         try:
-            end_date_input = input("Zadajte dátum ukončenia zmluvy: ").strip()
-            end_date = datime.strptime(end_date_input, "%d-%m-%y")
+            termination_date_input = input("Zadajte dátum ukončenia zmluvy: (YYYY-MM-DD) ").strip()
+            termination_date = datetime.strptime(termination_date_input, "%Y-%m-%d")
+
+            if termination_date < self.start_date:
+                print("Neplatný dátum ukončenia.")
+                return
+
+            if termination_date >= self.end_date:
+                print(f"zmluva je platná len do {self.end_date}")
+                return
+
+            days_worked = (termination_date - self.start_date).days
+            days_remaining_part = self.contract_duration - days_worked
+            amount_day_net = self.total_paid/self.contract_duration
+            refund_amount = amount_day_net*days_remaining_part
+
+            self.print_summary(termination_date, days_worked, days_remaining_part, refund_amount)
+            return refund_amount
+
+        except ValueError:
+            print("Neplatný formát dátumu.")
+            return
+
+
+    def print_summary(self, termination_date, days_worked, days_remaining_part, refund_amount):
+        print("\nPredčasné ukončenie zmluvy - Zhrnutie")
+        print(f"Začiatok zmluvy: {self.start_date.strftime('%d.%m.%Y')}")
+        print(f" Plánovaný koniec zmluvy: {self.end_date.strftime('%d.%m.%Y')}")
+        print(f"Skutočný dátum ukončenia: {termination_date.strftime('%d.%m.%Y')}")
+        print(f" Vyplatená suma: {self.total_paid:.2f} EUR")
+        print(f"Odpracované dni: {days_worked}")
+        print(f"Zostávajúce dni: {days_remaining_part}")
+        print(f"Vrátená suma zamestnávateľovi: {refund_amount:.2f} EUR")
+
+
+
+# Employee's contract
+start_date = datetime(2025, 1, 1)
+total_paid = 2000  # EUR
+
+# Create instance of calculator
+calc = CalculateRefund(start_date, total_paid)
+
+# Perform calculation
+calc.calculate_refund()
+
+
 
 
 
